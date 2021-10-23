73db9883ea3d7b72da868e2de0c60fa995a1bb1d
Marcel <marcelc6363@gmail.com>
2021-10-24 00:11:43 +0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,6 +29,18 @@ const MyApp: React.FC<AppProps> = (props: AppProps) => {
           href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LCVJMC05DE"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-47867706-3', { page_path: window.location.pathname });`,
+          }}
+        ></script>
       </Head>
       <ProtectRoute {...props} />
     </Provider>
